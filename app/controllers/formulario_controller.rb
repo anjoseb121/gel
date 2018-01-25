051e4a5aa166e20a0db21bb2e1b9b603e9901a50
humatios <humatios@hotmail.com>
2018-01-25 17:00:51 -0500

    Fix message in forms

--- app/controllers/formulario_controller.rb
+++ app/controllers/formulario_controller.rb
@@ -1,5 +1,6 @@
 class FormularioController < ApplicationController
   def index
     @formulario = Formulario.new(params)
+    redirect_to request.env['HTTP_REFERER'], flash: {response_data: @formulario.attr_response}
   end
 end
